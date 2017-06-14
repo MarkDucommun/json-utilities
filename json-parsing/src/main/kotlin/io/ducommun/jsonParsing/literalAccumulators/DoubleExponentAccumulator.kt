207a01cf87b87b088ad7c6c2d4244fd4275846d6
yourBusiness <noneOf>
2017-06-13 23:17:51 -0500

    separate a few accumulators into their own files

--- json-parsing/src/main/kotlin/io/ducommun/jsonParsing/literalAccumulators/DoubleExponentAccumulator.kt
+++ json-parsing/src/main/kotlin/io/ducommun/jsonParsing/literalAccumulators/DoubleExponentAccumulator.kt
@@ -4,14 +4,14 @@ import com.hcsc.de.claims.results.Failure
 import com.hcsc.de.claims.results.Result
 import com.hcsc.de.claims.results.Success
 
-data class DoublePointAccumulator(
+data class DoubleExponentAccumulator(
         val value: String
 ) : Accumulator {
 
     override fun addChar(char: Char): Result<String, Accumulator> {
 
         return when (char) {
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> Success(DoubleAccumulator(value = value + char))
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-' -> Success(DoubleAccumulator(value = value + char))
             else -> Failure("Invalid JSON - '$char' may not be part of a number")
         }
     }
